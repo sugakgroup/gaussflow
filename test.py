9ad2fc874062a5045ed9a9c915aec9c42c178b92
sgknsk526 <suga@shuyu.kuchem.kyoto-u.ac.jp>
2026-06-18 18:12:46 +0900

    feat: Update .gitignore and remove test script

--- test.py
+++
@@ -1,5 +0,0 @@
-from gaussflow.gaussian_parser import analyze_g16_output
-from pathlib import Path
-
-if __name__ == "__main__":
-    print(analyze_g16_output(id=0, base_dir=Path("output/mol_0"), wfname="s1t1sp", target_properties=["xyz","SCF_energy","s1-t1_gap","excitation_energy_s1","excitation_energy_t1"]))
